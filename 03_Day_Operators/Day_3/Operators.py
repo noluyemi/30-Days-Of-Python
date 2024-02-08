3bda121042389fb34bd1f3e4261175f02b1848ab
noluyemi <159228753+noluyemi@users.noreply.github.com>
2024-02-08 07:06:21 -0500

    Create Operators.py

---
+++ 03_Day_Operators/Day_3/Operators.py
@@ -0,0 +1,37 @@
+age = 23
+height = 1.65
+com_num = 9 + 8j
+
+base = int(input('Enter base:', ))
+height int(input('Enter height:', ))
+area = base * height
+print(area)
+
+side_a = int(input('Side a:', ))
+side_b = int(input('Side b:', ))
+side_c = int(input('Side c:', ))
+perimeter = side_a + side_b + side_c
+print('The perimeter of the triangle is', perimeter)
+
+length = int(input('length:' ))
+width = int(input('width:' ))
+area = length * width
+print(area)
+perimeter = 2 * (length + width)
+print(perimeter)
+
+radius = int(input('radius:' ))
+pi = 3.14
+area = pi * (radius ** 2)
+print(area)
+circumference = 2 * pi * radius
+print(circumference)
+
+
+
+
+
+          
+
+
+    
