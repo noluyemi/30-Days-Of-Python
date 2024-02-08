7e7ebde56af4f20951372680cdbf114c72590c8c
noluyemi <159228753+noluyemi@users.noreply.github.com>
2024-02-08 06:38:51 -0500

    Create variables.py

---
+++ 02_Day_Variables_builtin_functions/day_2/variables.py
@@ -0,0 +1,36 @@
+# Day 2: 30 Days of python programming 
+first_name = 'Nolan'
+last_name = 'Reeves'
+full_name = 'Alan Mike'
+country = 'France'
+city = 'Paris'
+age = 12
+year = 2001
+is_married = True
+is_true = True
+is_light_on = False
+
+
+print(type(first_name))
+print(len(first_name))
+num_one = 5
+num_two = 4
+total = num_one + num_two
+product = num_two * num_one
+variable_division = num_one / num_two
+variable_remainder = num_two % num_one
+variable_exp = num_one ** num_two
+floor_division = num_one // num_two
+
+radius = 30
+area = math.pi * (radius ** 2)
+print(area)
+
+
+
+
+
+
+
+
+
